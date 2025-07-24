871262f970728b90ddd292dffb1bbcc1e42077ac
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-07-24 12:39:51 -0400

    Update Reporting.sql

--- Reporting.sql
+++ Reporting.sql
@@ -15,3 +15,56 @@ GROUP BY sport
 ORDER BY events DESC;
 
 ---------------------------------------Ch.2 Creating Reports---------------------------------------
+WHERE -- Used when filtering on a dimension
+HAVING -- Used when filtering on an aggregation, such as SUM(revenue)
+
+-- Option A: JOIN first, UNION second
+SELECT
+athlete_id,
+gender,
+age,
+gold
+FROM summer_games AS sg
+JOIN athletes AS a
+ON sg.athlete_id = a.id;
+-----------------------------Now combine with UNION
+UNION ALL
+SELECT
+athlete_id,
+gender,
+age,
+gold
+FROM winter_games AS wg
+JOIN athletes AS a
+ON wg.athlete_id = a.id;
+
+-- Option B: UNION first, JOIN second
+SELECT
+athlete_id,
+gold
+FROM summer_games AS sg
+UNION
+SELECT
+athlete_id,
+gold
+FROM winter_games AS wg;
+--------------------------Now turn this into subquery
+SELECT
+athlete_id,
+gender,
+age,
+gold
+  
+FROM (
+  SELECT
+  athlete_id,
+  gold
+  FROM summer_games AS sg
+  UNION
+  SELECT
+  athlete_id,
+  gold
+  FROM winter_games AS wg) AS g
+JOIN athletes AS a
+ON g.athlete_id = a.id;
+
