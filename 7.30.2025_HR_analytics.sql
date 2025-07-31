df03313ca5d8bd3afceefba0ad3cdd27b719dbe0
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-07-30 23:02:08 -0400

    Create 7.30.2025_HR_analytics.sql

---
+++ 7.30.2025_HR_analytics.sql
@@ -0,0 +1,8 @@
+/* Monitor HR Metrics and Understand what factors impact Attrition
+1. Connect and Prepare Data
+2. Build Charts
+3. Create Dashboards
+4. Create Stories
+
+Employee - performance rating | employee id = employee id one to many
+Employee - education level | Many to One Education = education level ID
