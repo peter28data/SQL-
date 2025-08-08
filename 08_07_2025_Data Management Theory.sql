3292416dffb0955c261a026d41174912f1721f7d
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-08-07 22:56:23 -0400

    Create 08_07_2025_Data Management Theory.sql

---
+++ 08_07_2025_Data Management Theory.sql
@@ -0,0 +1,28 @@
+/*
+AWS DynamoDB
+Offers low latency + high throughput
+ideal for a mobile application that requires a managed NoSQL database service to store user data in a flexible, JSON-like format.
+
+
+Apache Airflow - Google Cloud Composer
+Schedule sql queries to run daily as directed acyclic graph and generate the insights daily
+
+
+Google BigQuery
+Scales inernationally and cloud data warehouse
+
+
+Data Flow
+Azure Data Factory
+By using the data flow component in azure data factory, a business can define a series of transformations to apply to its data as it moves from the source to the destination. 
+
+
+Amazon S3
+Amazon Simple Storage Service
+Handles cloud storage of large amount of unstructured text data
+
+
+Second Normal Form (2NF)
+Create a separate table for the products and their attributes
+-1NF is no repeating groups, each field contains only atomic values.
+-2NF all non-key attributes must be fully functionally dependent on the entire primary key
