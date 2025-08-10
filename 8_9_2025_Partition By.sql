21e78cbae84695c7612dd9ea182fc4a56604cf6d
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-08-09 22:47:38 -0400

    Update 8_9_2025_Partition By.sql

--- 8_9_2025_Partition By.sql
+++ 8_9_2025_Partition By.sql
@@ -2,9 +2,8 @@
 --Explanation: selecting country name with a window function, this query will return the season avg, per year, for each country name. 
 SELECT
 c.country_name,
-m.season_year,r
-  
-AVG(home_goal + away_goal)
+m.season_year, 
+AVG(home_goal + away_goal) 
   OVER(PARTITION BY m.season_year, c.country_name) AS season_avg_percountry --Window Function
 
 FROM countries AS c
@@ -14,3 +13,9 @@ ON c.id = m.country_id
 ----------------------------------------------------------------------------------------------
 -- Sliding Windows
 -- Perform calculations relative to the Current Row
+
+PRECEDING           -- Specify number of rows before input
+FOLLOWING           -- Specify number of rows after input
+  
+UNBOUNDED PRECEDING -- Every row since the beginning
+UNBOUNDED FOLLOWING -- Every row to the end
