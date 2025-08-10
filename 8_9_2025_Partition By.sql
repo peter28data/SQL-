3be45533ca9915b2b1ccd142009dd28f3260bc68
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-08-09 21:14:40 -0400

    Create 8_9_2025_Partition By.sql

---
+++ 8_9_2025_Partition By.sql
@@ -0,0 +1,16 @@
+-- Window Function: OVER(PARTITION BY x, y)
+--Explanation: selecting country name with a window function, this query will return the season avg, per year, for each country name. 
+SELECT
+c.country_name,
+m.season_year,r
+  
+AVG(home_goal + away_goal)
+  OVER(PARTITION BY m.season_year, c.country_name) AS season_avg_percountry --Window Function
+
+FROM countries AS c
+LEFT JOIN match_resulsts AS m
+ON c.id = m.country_id
+
+----------------------------------------------------------------------------------------------
+-- Sliding Windows
+-- Perform calculations relative to the Current Row
