45318917b2f255d33c25c30ee19eb32b15d26f84
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-08-10 23:27:10 -0400

    Update 8_9_2025_Partition By.sql

--- 8_9_2025_Partition By.sql
+++ 8_9_2025_Partition By.sql
@@ -76,4 +76,29 @@ LEFT JOIN home ON m.id = home.id
 LEFT JOIN away ON m.id = away.id
 
 
--- Create the 
+-- Create the Two Common Table Expressions
+WITH home AS (
+  SELECT
+  m.id, 
+  t.team_long_name,
+  CASE 
+  WHEN m.home_goal > m.away_goal THEN 'MU Win'
+  WHEN m.home_goal < m.away_goal THEN 'MU Loss'
+  ELSE 'Tie' END AS outcome
+  FROM match AS m
+  LEFT JOIN team AS t
+  ON m.hometeam_id = t.team_api_id),
+
+away AS (
+  SELECT
+  m.id, 
+  t.team_long_name,
+  CASE
+  WHEN m.home_goal > m.away_goal THEN 'MU Loss'
+  WHEN m.home_goal < m.away_goal THEN 'MU Win'
+  ELSE 'Tie' END AS outcome
+  FROM match AS m
+  LEFT JOIN team AS t
+  ON m.awayteam_id = t.team_api_id)
+
+-- Organize the queries together starting with the CTEs followed by the selecting query, then the Joins, ending with the filtering.
