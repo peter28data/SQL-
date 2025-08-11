9690b603cf79437a45b7dffb9ffffd73e41f6172
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-08-10 23:38:26 -0400

    Update 8_10_2025_Cleaning Data.sql

--- 8_10_2025_Cleaning Data.sql
+++ 8_10_2025_Cleaning Data.sql
@@ -7,4 +7,17 @@ trim(street, '0123456789 #/.') AS cleaned_streetname
 FROM evanston311
 ORDER BY street;
 
+--------------------------------------------------------------------------
+-- Unstructured Text
+-- The goal is to find the total sum of inquires that mention trash or garbarge in the description WITHOUT the context of trash/garbage being in the category.
 
+SELECT category, count(*)
+  FROM evanston311 
+ WHERE (description ILIKE '%trash%'
+    OR description ILIKE '%garbage%') 
+   AND category NOT LIKE '%Trash%'
+   AND category NOT LIKE '%Garbage%'
+ -- What are you counting?
+ GROUP BY category
+ ORDER BY count DESC
+ LIMIT 10;
