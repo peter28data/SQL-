6f46d46deb0db9920f6f907eb41d05d141600f7b
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-08-11 22:41:37 -0400

    Create 8_11_2025_Date Formats.sql

---
+++ 8_11_2025_Date Formats.sql
@@ -0,0 +1,25 @@
+-- Date Comparisons
+-- The format will return either a 0 or 49 requests. 
+SELECT COUNT(*)
+FROM evanston311
+WHERE date_created = '2018-01-01';
+
+-- The query above will not return anything. Howevery when we convert the query to a timestamp it will return 49 requests.
+SELECT '2018-01-02'::timestamp;
+
+----------------------------------------------------------
+
+-- Number of Requests
+SELECT count(*) 
+FROM evanston311
+WHERE date_created::DATE = '2017-01-31';
+
+-- Number of Requests
+-- Greater than or equal to Feb 29 but less than March 1st queries 
+SELECT count(*)
+FROM evanston311
+WHERE date_created >= '2016-02-29'
+AND date_created < '2016-03-01';
+
+---------------------------------------------------------
+
