41a852e580a288bbe48e30c55f4a553b44c235c6
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-08-16 23:02:56 -0400

    Update 8_13_2025_Reporting.sql

--- 8_13_2025_Reporting.sql
+++ 8_13_2025_Reporting.sql
@@ -137,7 +137,37 @@ WHERE athlete_id IN
 
 -------------------------------------------------------------------------------
 
+-- Top Athletes in Nobel-Prized countries
+SELECT 
+  event,
+  CASE
+  WHEN event LIKE '%Women%' THEN 'female'
+  ELSE 'male' END AS gender,
+  COUNT(DISTINCT athlete_id) AS athletes
+FROM summer_games
+WHERE country_id IN (
+  SELECT
+  country_id
+  FROM country_stats
+  WHERE nobel_prize_winners > 0)
+GROUP BY event
+UNION
+SELECT
+  event,
+  CASE 
+  WHEN event LIKE '%Women%' THEN 'female'
+  ELSE 'male' END AS gender,
+  COUNT(DISTINCT athlete_id) AS athletes
+FROM winter_games
+WHERE country_id IN (
+  SELECT country_id
+  FROM country_stats
+  WHERE nobel_prize_winners > 0)
+GROUP BY event
+ORDER BY athletes DESC
+LIMIT 10;
 
+------------------------------------------------------------------------------
 
 
 
