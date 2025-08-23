2ce36e307cd66ee8e3421a3c8ee582877b545c59
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-08-23 17:17:47 -0400

    Create 8_23_2025_File_Folder_Structure.sql

---
+++ 8_23_2025_File_Folder_Structure.sql
@@ -0,0 +1,20 @@
+/*
+
+The requirements.txt
+-A list of libraries to be imported
+-numpy==1.19.5
+-pandas==1.4.0
+-matplotlib==3.5.0
+-seaborn==0.11.2
+
+Explanation: The libraries and version of each are required in this file.
+
+-------------------------------------
+
+To Create a New Directory 
+-This makes a new folder in the main branch.
+
+mainbranch_name/newbranch_name/file_newbranch.sql
+
+-------------------------------------
+
