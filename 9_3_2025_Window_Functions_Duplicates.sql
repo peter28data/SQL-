ca29fd7fdd9951e8464090c4abfcbc40029077d8
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-09-07 12:18:02 -0400

    Update 9_3_2025_Window_Functions_Duplicates.sql

--- 9_3_2025_Window_Functions_Duplicates.sql
+++ 9_3_2025_Window_Functions_Duplicates.sql
@@ -38,11 +38,71 @@ WHERE row_number = 1   -- Keep only Unique rows
 
 -- CASE WHEN will lowercase all values in the order status to use the LIKE operater to return anything that may have misspelled as "deliver, return, refund, pend, or ship" and return each as a proper case.
 
+
+-- Final Query
+  
 WITH cleaned_data AS (
   SELECT
   order_id,
-  INITCAP
 
+  -- Clean customer name
+  INITCAP(customer_name) AS customer_name,
+  email,
+
+  -- Standardize order_status
+  CASE
+    WHEN LOWER(order_status) LIKE '%deliver%' THEN "Delivered"
+    WHEN LOWER(order_status) LIKE '%return%' THEN "Returned"
+    WHEN LOWER(order_status) LIKE '%refund%' THEN "Refunded"
+    WHEN LOWER(order_status) LIKE '%pend%' THEN "Pending"
+    WHEN LOWER(order_status) LIKE '%ship%' THEN "Shipped"
+    ELSE 'Other'
+  END AS cleaned_order_status,
+
+  --Standardize product_name
+  CASE
+    WHEN LOWER(product_name) LIKE '%apple watch%' THEN "Apple Watch"
+    WHEN LOWER(product_name) LIKE '%samsung galaxy s22%' THEN "Samsung Galaxy S22"
+    WHEN LOWER(product_name) LIKE '%google pixel%' THEN "Google Pixel"
+    WHEN LOWER(product_name) LIKE '%macbook pro%' THEN "MacBook Pro"
+    WHEN LOWER(product_name) LIKE '%iphone 14%' THEN "iPhone 14"
+    ELSE 'Other'
+  END AS cleaned_product_name,
+
+  -- Clean quantity
+  CASE
+    WHEN LOWER(quantity) = 'two' THEN 2
+    ELSE SAFE_CAST(quantity) AS INT64)
+    END AS clean_quantity,
+
+  --Standardize date
+  COALESCE(
+    SAFE.PARSE_DATE('%Y-%m-%d', CAST(order_date AS STRING)),
+    SAFE.PARSE_DATE('%m/%d/%Y', CAST(order_date AS STRING))
+  ) AS standardized_order_date
+
+FROM product_inventory_table
+WHERE customer_name IS NOT NULL
+),
+
+deduplicated_data AS (
+  SELECT *,
+    ROW_NUMER() OVER (
+      PARTITION BY LOWER(email), LOWER(clean_product_name)
+      ORDER BY order_id
+    ) AS row_number
+  FROM cleaned_data
+),
+
+final_table AS (
+SELECT *
+FROM deduplicated_data
+WHERE row_number = 1
+)
+
+SELECT * FROM final_table
+
+-------------------------------------
 
 
 
