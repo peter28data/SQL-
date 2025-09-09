0a7239817035fb01a7d5481ff437ab2cab76f781
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-09-08 21:32:12 -0400

    Update 9_7_2025_Cleaning Data.sql

--- 9_7_2025_Cleaning Data.sql
+++ 9_7_2025_Cleaning Data.sql
@@ -30,14 +30,14 @@ UPPER(product_name) = product_name;
 
 --------------------Step 2: Fix the Leak -----------------
 
--- 2.1 Missing Data
+-- Missing Data
 -- It can present itself as 0, "0", blank fields, "Not Specified", "N/A", "#N/A", None, NaN, NULL, or Infinity.
 
--- Drop Rows where Null
+-- 2.1 Drop Rows where Null
 DELETE FROM your_table
 WHERE product_review IS NULL;
 
--- Create new Column
+-- 2.2 Create new Column
 -- An empty "last_purchase_date" can be translated to a "purchased_recently" column, with values 0 for "no" and 1 for "yes".
 ALTER TABLE your_table
 ADD purchased_recently INT;
@@ -48,8 +48,7 @@ ELSE 1
 END;
 
 
--- Estimate Values with Rolling Averages or Forward Fils
-
+-- 2.3 Estimate Values with Rolling Averages or Forward Fils
 -- Fill with Average
 UPDATE your_table
 SET temperature = (
@@ -60,8 +59,40 @@ SET temperature = (
 WHERE temperature IS NULL;
 
 
---
+-- Outliers
+-- Use Case: We detect grocery shoppers spending $0.05 annually which is unusually low and may affect the majority of the data when calculated averages.
 
+-- 2.4 Remove Rows where purchase_amount is in the bottom 1% such as 5 cents or top 1% for extreme shoppers.
+DELETE FROM your_table
+WHERE purchase_amount > PERCENTILE_CONT(0.99)
+
+WITHIN GROUP (ORDER BY purchase_amount) OR
+
+purchase_amount < PERCENTILE_CONT(0.01)
+WITHIN GROUP (ORDER BY purchase_amount);
+
+
+-- 2.5 Parition The Data
+-- Segregate the standard entries from the outliers by creating a new column
+ALTER TABLE your_table ADD segment VARCHAR(50);
+UPDATE your_table SET segment = CASE
+WHEN purchase_amount > SOME_THRESHOLD THEN 'Outlier'
+ELSE 'Standard'
+END;
+
+
+-- 2.6 Retain Outliers, Adapt Analysis 
+-- Calculate a trimmed mean for 'purchase_amount', excluding the top and bottom 5%
+SELECT AVG(purchase_amount)
+FROM (
+  SELECT purchase_amount
+  FROM your_table
+  ORDER BY purchase_amount
+  LIMIT 5% OFFSET 5%
+  ) AS trimmed_data;
+
+
+--
 
 
 
