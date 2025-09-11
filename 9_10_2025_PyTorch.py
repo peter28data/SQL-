bdc03c928e6b85c4053060b5fb4612810b14df89
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-09-10 23:13:10 -0400

    Create 9_10_2025_PyTorch.py

---
+++ 9_10_2025_PyTorch.py
@@ -0,0 +1,11 @@
+# ------------------------------------- Ch.1: Deep Learning with PyTorch -------------------------------------
+
+# Lists into Tensors
+import torch
+my_list = [[1,2,3], [4,5,6]]
+tensor = torch.tensor(my_list)
+
+print(tensor)
+
+
+# 
