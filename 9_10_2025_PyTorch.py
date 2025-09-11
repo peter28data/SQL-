177e740e58901e3cf3eb8ab32bbaf836cf3b94b9
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-09-10 23:19:08 -0400

    Update 9_10_2025_PyTorch.py

--- 9_10_2025_PyTorch.py
+++ 9_10_2025_PyTorch.py
@@ -3,9 +3,20 @@
 # Lists into Tensors
 import torch
 my_list = [[1,2,3], [4,5,6]]
+
 tensor = torch.tensor(my_list)
-
 print(tensor)
+print(tensor.shape)
+print(tensor.dtype)
 
 
-# 
+# Matrix Multiplication
+a = torch.tensor([[1, 1],
+                [2, 2]])
+b = torch.tensor([[2, 2],
+                  [3, 3]])
+print(a @ b)
+# Returns: tensor([[5, 5], [10, 10]])
+
+
+# --------------------------------------------------------------------------
