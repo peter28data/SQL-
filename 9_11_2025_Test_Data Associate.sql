041f27afc074845c3bcdcdad73779ae82a5f07e4
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-09-11 23:48:05 -0400

    Update 9_11_2025_Test_Data Associate.sql

--- 9_11_2025_Test_Data Associate.sql
+++ 9_11_2025_Test_Data Associate.sql
@@ -14,9 +14,95 @@ SELECT
   (3 * (mean - median))/std_dev AS Pearson_Coefficient
 FROM sub_table;
 
+--------------------------------------------------------------------------
+
+
+-- Calculate Median
+SELECT
+  PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY Tenure) AS Median_Tenure
+FROM customer;
 
 --------------------------------------------------------------------------
 
 
---
+-- Identify Usernames
+-- Each username contains lowercase letters, digits, and some special characters, create a query to identify users whose username ends with a digit given the possible characters
+SELECT
+  username, first_name, last_name
+FROM users
+WHERE username ~ '[0-9]*';     --??
+
+--------------------------------------------------------------------------
+
+
+-- Get the Data Type
+SELECT
+  pg_typeof(amount_paid) AS "Data Type"    -- Double quotes perserves the space and proper case
+FROM customers
+
+--------------------------------------------------------------------------
+
+
+-- Change the table
+-- From CHAR data type to VARCHAR data type
+ALTER TABLE employee
+ALTER COLUMN employee_name TYPE VARCHAR
+
+--------------------------------------------------------------------------
+
+
+-- Return Month
+-- In letter format such as september whereas extract would return the number format such as 9
+TO_CHAR(event_time, 'Month') AS month_name
+
+--------------------------------------------------------------------------
+
+
+-- Return Month
+-- In number format
+EXTRACT(MONTH FROM sale_date)
+
+--------------------------------------------------------------------------
+
+
+-- oneline gaming service
+-- information about each usernames remaining credits and account creation, write a query to view the average amount of credits for users whose accounts were created in each year
+SELECT 
+  TO_CHAR(created, 'YYYY') AS year,
+  credits::DECIMAL (4,2) AS mean_credit
+from accounts
+group by year
+order by year;
+
+---------------------------------------------------------------------------------------------------------------
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
