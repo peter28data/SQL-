74908c795392e4d4ba931daef89f9a254bc7d3c5
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-09-15 13:01:25 -0400

    Update 9_11_2025_Test_Data Associate.sql

--- 9_11_2025_Test_Data Associate.sql
+++ 9_11_2025_Test_Data Associate.sql
@@ -4,7 +4,8 @@
 -- Using a CTE to find the average sales price for all the years in the retail table for multiple retail locations to calculate the Pearson coefficient in the main query.
 WITH sub_table AS (
   SELECT
-    PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY price ASC) AS median,
+    PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY price ASC) AS median, -- Calculate Median
+
     AVG(price) AS mean,
     STDDEV(price) AS std_dev
   FROM retail
@@ -17,20 +18,12 @@ FROM sub_table;
 --------------------------------------------------------------------------
 
 
--- Calculate Median
-SELECT
-  PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY Tenure) AS Median_Tenure
-FROM customer;
-
---------------------------------------------------------------------------
-
-
 -- Identify Usernames
--- Each username contains lowercase letters, digits, and some special characters, create a query to identify users whose username ends with a digit given the possible characters
+-- Each username contains lowercase letters, digits, and some special characters, create a query to identify users whose username Ends with a digit given the possible characters
 SELECT
   username, first_name, last_name
 FROM users
-WHERE username ~ '[0-9]$';     --??
+WHERE username ~ '[0-9]$';     -- john9x would not work bc it ends with x, the '$'  matches the end
 
 --------------------------------------------------------------------------
 
@@ -51,38 +44,31 @@ ALTER COLUMN employee_name TYPE VARCHAR
 --------------------------------------------------------------------------
 
 
--- Return Month
--- In letter format such as september whereas extract would return the number format such as 9
-TO_CHAR(event_time, 'Month') AS month_name
-
---------------------------------------------------------------------------
-
-
--- Return Month
--- In number format
-EXTRACT(MONTH FROM sale_date)
-
---------------------------------------------------------------------------
-
-
--- oneline gaming service
+-- Online Gaming service
 -- information about each usernames remaining credits and account creation, write a query to view the average amount of credits for users whose accounts were created in each year
 SELECT 
   TO_CHAR(created, 'YYYY') AS year,
   credits::DECIMAL (4,2) AS mean_credit
 from accounts
-group by year
+group by year    -- Can only group by year since it is a text data type
 order by year;
 
 ---------------------------------------------------------------------------------------------------------------
   
-
 -- 2 TYPES of Functions
 SELECT 
+  -- Letter Format: Converts to String
+  TO_CHAR(created, 'Day') AS day    -- Returns: Friday
+  -- Numer Format: PostgreSQL specific funtion
   DATE_PART('hour', date_created) AS hour,
+  -- Number Format: Standard SQL function
   EXTRACT(month FROM date_created) AS month
+  -- Extract Title: Can be done by splitting the string
+  SPLIT_PART(title,':',1) AS name,    -- variable, delimiter, before or after
+  
 FROM orders
 
+--------------------------------------------------------------------------
 
 
 
