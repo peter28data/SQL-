04380a51cc56e9e74a6838dde0a7279a609b7b04
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-09-18 14:53:49 -0400

    Update 9_18_2025_Analyze Data.sql

--- 9_18_2025_Analyze Data.sql
+++ 9_18_2025_Analyze Data.sql
@@ -15,7 +15,7 @@ profit_by_city = df.groupby("city")["profit"].sum()
 profit_by_city = profit_by_city.sort_values(ascending=False)
 print(profit_by_city)
 
-----------------------------------------------------------------------------
+--------------------- Part 2: Number of Customer Orders---------------------------
 
 -- Number of Order per Customer
 -- The Goal: Find the customers who order the most products.
@@ -32,3 +32,40 @@ ORDER BY number_of_orders DESC
 order_per_customer = df.groupby("customer_name")["order_id"].nunique()
 order_per_customer = profit_by_city.sort_values(ascending=False)
 print(order_per_customer)
+
+--------------------------------------------------------------------------
+
+-- Tableau Summary
+-- By putting customer name in the rows and the order id in the columns, you get all the order id's as column and nothing in the values. To fix this hover the order id to get a dropdown and select measure then count(distinct). Then we hover the order id again to select sort, by field, and descending. 
+
+
+
+------------------ Part 3:  Profitability of Sub-Categories--------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
