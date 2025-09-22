cf65a3c7b62ed701d1a6ff525be524f9324a8a9c
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-09-22 09:40:42 -0400

    Update 9_18_2025_Analyze Data.sql

--- 9_18_2025_Analyze Data.sql
+++ 9_18_2025_Analyze Data.sql
@@ -1,4 +1,4 @@
--------------------------- Part 1: Analyze Profit by City --------------------
+-------------------------- Task 1: Analyze Profit by City ------------------
 
 -- Total Profit by City
 
@@ -15,7 +15,7 @@ profit_by_city = df.groupby("city")["profit"].sum()
 profit_by_city = profit_by_city.sort_values(ascending=False)
 print(profit_by_city)
 
---------------------- Part 2: Number of Customer Orders---------------------------
+--------------------- Task 2: Number of Customer Orders---------------------
 
 -- Number of Order per Customer
 -- The Goal: Find the customers who order the most products.
@@ -40,14 +40,58 @@ print(order_per_customer)
 
 
 
------------------- Part 3:  Profitability of Sub-Categories--------------------
+------------------ Task 3:  Profitability of Sub-Categories-----------------
 
+SELECT
+  "Sub-Category",
+  SUM(profit) AS total_profit
+FROM retail_stores
+GROUP BY "Sub-Category"
+HAVING SUM(profit) > 5000
+ORDER BY total_profit DESC;
 
+--------------------------------------------------------------------------
 
+-- Python Code
+sub_category_profit = df.groupby("Sub-Category")["Profit"].sum()
+profitability_sub_categories = sub_category_profit[sub_category_profit > 50000]
 
+profitability_sub_categories = profitability_sub_categories.sort_values(ascending=False)
+print(profitability_sub_categories)
 
+------------------ Task 4: Sales Trend Over Time ---------------------------
 
+SELECT
+  FORMAT_DATE('%Y-%m', "Order Date") AS order_month,
+  SUM(sales) AS total_sales
+FROM retail_stores
+GROUP BY order_month
+ORDER BY order_month;
 
+--------------------------------------------------------------------------
+
+-- Python Code
+df['Order Date'] = pd.to_datetime(df['Order Date'])
+df['Order Date'] = df['Order Date'].dt.to_period('M')
+monthly_sales = df.groupby('Order Month')['Sales'].sum()
+print(monthly_sales)
+
+-- Simple Line Chart
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(15,7))
+monthly_sales.plot(kind='line', marker='o')
+
+-- Adding plot title and label
+plt.title('Monthly Sales Trend Over Time')
+plt.xlabel('Order Month')
+plt.ylabel('Total Sales')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+plt.show()
+
+--------------------------------------------------------------------------
 
 
 
