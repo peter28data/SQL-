409e506617b7f2f20975fdb004262d6d8afd70ca
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-09-29 23:54:35 -0400

    Update 1_applyingSQL.sql

--- 1_applyingSQL.sql
+++ 1_applyingSQL.sql
@@ -28,6 +28,7 @@ GROUP BY active;
 
 
 
+
 --#2 Aggregating Strings: 
 --Demonstrate family-friendly/multi-lingual films
 --The query below will return a column with a list of all the different languages following by a column including all the film titles that are available in that language. This query demonstrates only films released in 2010 with a 'G' rating by combining two tables on the primary key 'language_id'.
