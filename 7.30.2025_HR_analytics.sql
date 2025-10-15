345589395bf033c9723f12ff2989394a867b95c6
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-10-14 22:47:19 -0400

    Update 7.30.2025_HR_analytics.sql

--- 7.30.2025_HR_analytics.sql
+++ 7.30.2025_HR_analytics.sql
@@ -6,3 +6,28 @@
 
 Employee - performance rating | employee id = employee id one to many
 Employee - education level | Many to One Education = education level ID
+
+*/
+
+
+
+
+---------------- Task 1: Employee Attrition -----------------------
+
+-- Key Insights 
+-- Atlas Labs LLC has employed 1470 people.
+-- Currently employes over 1200 people.
+
+-- The largest department is Technology.
+-- The Attrition Rate for employees leaving the organization across departments is 16%.
+
+-- Majority of employees are between 20-29 yrs old.
+-- Atlas Labs employes 2.7% more women than men.
+
+-- Non-binary make up 8.5%.
+-- White have the highest average salary
+-- Mixed or multiple ethnic groups have the lowest average salaries.
+
+
+
+
