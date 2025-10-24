0bf8fcf4ba775b04bd5b44b6e084b9eab45e90fe
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-10-23 23:49:42 -0400

    Document schemas and databases in Snowflake.sql
    Add comments detailing schemas and databases in Snowflake.

---
+++ 10_23_2025 Snowflake.sql
@@ -0,0 +1,14 @@
+-----------------------------------------------------------------
+
+--What are the schemas in the Database? 
+-- informa_schem, notebooks, student_store
+-- Database > Schemas > Tables?
+
+
+
+
+-----------------------------------------------------------------
+
+-- 3 Databases
+-- 'Course', 'Snowflake', 'User'
+-- All accessed through a User Interface (UI) Tool, Snowsight, to interact with Snowflake.
