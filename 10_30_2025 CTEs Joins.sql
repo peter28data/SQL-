15b185e997d97790f9a9c8230ac6f81ccea198b3
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-10-30 23:46:30 -0400

    Add SQL query for popular bike stations

---
+++ 10_30_2025 CTEs Joins.sql
@@ -0,0 +1,37 @@
+-----------------------------------------------------------------
+
+-- Most Popular Bike Stations by Start & End
+
+-- Start Stations
+WITH s AS (
+  SELECT start_station,
+  COUNT(start_date) AS st
+  FROM trip
+  GROUP BY start_station),
+
+  -- End Stations
+e AS (
+  SELECT end_station,
+  COUNT(end_date) AS end
+  FROM trip
+  GROUP BY end_station)
+
+-- Main Query
+SELECT 
+s.start_station AS station,
+s.st AS starts,
+e.end AS ends
+
+-- Joins
+FROM s
+INNER JOIN e
+ON s.start_station = e.end_station
+ORDER BY starts DESC
+LIMIT 3;
+
+
+
+
+-----------------------------------------------------------------
+
+-- Larger Market from Subscribers or Non-subscribers?r
