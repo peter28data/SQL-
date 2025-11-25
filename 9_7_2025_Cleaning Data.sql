c20f606c76a0c83c0f4d4f8ba0dae0f54e80474d
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-11-24 22:36:39 -0500

    Update 9_7_2025_Cleaning Data.sql

--- 9_7_2025_Cleaning Data.sql
+++ 9_7_2025_Cleaning Data.sql
@@ -21,6 +21,7 @@ FROM your_table
 WHERE gender NOT IN ('Male', 'Female');
 
 
+
 -- 1.4 Check for Uniformity
 -- One value make be upper case and in another lowercase. To check the data for inconsistent casing we use this function
 SELECT DISTINCT product_name
