4cb19eabe94b977b3640954bdc2faeac011f86ca
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-12-07 23:11:03 -0500

    Update 2_Analyzing Business Data.sql

--- 2_Analyzing Business Data.sql
+++ 2_Analyzing Business Data.sql
@@ -162,14 +162,22 @@ WITH user_monthly_activity AS (
   user_id
   FROM orders)
 SELECT
+  
 ROUND(COUNT(DISTINCT current.user_id) :: NUMERIC /
   GREATEST(COUNT(DISTINCT previous.user_id),1),2) AS retention_rate
+  
 FROM user_monthly_activity AS previous
 LEFT JOIN user_monthly_activity AS current
+  
 ON previous.user_id = current.user_id
 AND previous.delivr_month = (current.delivr_month - INTERVAL '1 month')
+  
 GROUP BY previous.delivr_month
 ORDER BY previous.delivr_month ASC;
+
+
+
+
 
 ------------------------------------Ch.3: ARPU, Histograms, Percentiles ---------------------------
 /* Unit Economics: Measures performance per unit, as opposed to overall performance
