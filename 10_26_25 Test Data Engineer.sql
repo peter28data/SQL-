ddc27ce2e422ef629ecee2aeb7231e7cbae71f32
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-12-20 23:58:45 -0500

    Update 10_26_25 Test Data Engineer.sql

--- 10_26_25 Test Data Engineer.sql
+++ 10_26_25 Test Data Engineer.sql
@@ -11,6 +11,7 @@
 
 
 
+
 -----------------------------------------------------------------
 
 --which aws service is most appropriate to automatically move objects between storage classess based on access patterns? S3 Intelligent-Tiering
