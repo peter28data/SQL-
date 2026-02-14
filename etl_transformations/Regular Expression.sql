ef61550ae11ddd71d428ee26f71866b9f6c2757a
Peter Garay-Robles <peter.garayrobles@gmail.com>
2026-02-13 23:55:33 -0500

    Update Regular Expression.sql

--- etl_transformations/Regular Expression.sql
+++ etl_transformations/Regular Expression.sql
@@ -4,15 +4,12 @@
 
 ------------------------ Task 1: Remove Special Characters --------------------------------------------------
 
-
 UPDATE accounts
 SET username = REGEXP_REPLACE(username, '[^A-Za-z0-9]', '', 'g')
 
 -- [^A-Za-z0-9] = not a letter or number
 -- Replaces all with ''.
 -- "user!@#123" -> "user123"
-
-
 
 
 -------------------------- Task 2: Swap First/Last Name ------------------------------------------------
@@ -25,8 +22,6 @@ SET full_name = regexp_replace(full_name, '(\w+),\s*(\w+)', '\2 \1');
 -- "Doe, John" -> "John Doe"
 
 
-
-
 -------------------------- Task 3: Extract year from Dates ------------------------------------------------
 
 UPDATE orders
@@ -35,8 +30,6 @@ SET year_only = REGEXP_REPLACE(order_date::text, '(\d{4})-\d{2}-\d{2}', '\1')
 -- (\d{4}) = capture 4-digit year
 -- \1 = first capture group
 -- "2024-09-23" -> "2024"
-
-
 
 
 -------------------------- Task 4: Examples with Flags -----------------------------------------------
@@ -48,6 +41,4 @@ SET body = regexp_replace(body, 'sql', 'SQL', 'gi')
 -- "sql, Sql, sQL" -> "SQL, SQL, SQL"
 
 
-
 --------------------------------------------------------------------------
-
