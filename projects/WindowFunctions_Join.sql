933d101af744132c5546f5a59ec49f419a1b5e91
Peter Garay-Robles <peter.garayrobles@gmail.com>
2026-02-15 23:53:52 -0500

    Update WindowFunctions_Join.sql

--- projects/WindowFunctions_Join.sql
+++ projects/WindowFunctions_Join.sql
@@ -60,6 +60,7 @@ WHERE athlete_id IN
 -------------------------------------------------------------------------------
 
 -- Subquery for Top Athletes in Nobel-Prized countries
+
 SELECT 
   event,
   CASE
@@ -93,10 +94,8 @@ ORDER BY athletes DESC          -- Events with the most athletes appear first
 LIMIT 10;
 
 -- Explanation: The query combines data from Summer and Winter games and filters for countries with at least one nobel prized winner however this condition can be changed for other business cases with different requirements. The Insight from this query that the top 4 events with the most unique athletes are female dominated ranging from 56-50 women in the swimming women's 4x100 metres Medly Relay at 56. The first male dominated event is the men's 4x200 metres freestyle relay also for swimming. 
+
 ------------------------------------------------------------------------------
-
-
---------------------------------------------------------------------------------
 
 -- Total Medals by Population in Millions
 -- Replace Null for SUM function
@@ -476,20 +475,3 @@ ORDER BY gold_medals DESC;
 ----------------------------------
 
 -- Created on 8.13.2025
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
