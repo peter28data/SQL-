186eee97777be2da591598de0f51fdd5f8125c0f
Peter Garay-Robles <peter.garayrobles@gmail.com>
2026-02-19 23:28:23 -0500

    Update Partition By_RunningTotals.sql

--- projects/Partition By_RunningTotals.sql
+++ projects/Partition By_RunningTotals.sql
@@ -1,5 +1,8 @@
+--------------------------------------------------------------------------------------------
+
 -- Window Function: OVER(PARTITION BY x, y)
 --Explanation: selecting country name with a window function, this query will return the season avg, per year, for each country name. 
+
 SELECT
 c.country_name,
 m.season_year, 
@@ -10,7 +13,9 @@ FROM countries AS c
 LEFT JOIN match_resulsts AS m
 ON c.id = m.country_id
 
+
 ---------------------------------------------------------------------------------------------
+
 -- Sliding Windows
 -- Perform calculations relative to the Current Row
 
@@ -21,11 +26,12 @@ UNBOUNDED PRECEDING -- Every row since the beginning
 UNBOUNDED FOLLOWING -- Every row to the end
 
 CURRENT ROW         -- Stops the calculation at the Current Row
-
+  
 ---------------------------------------------------------------------------------------------
 
 -- Running Total from oldest to Most recent
 -- Calculates the sum of goals when Manchester city played as the home team during the 2011/2012 season
+
 SELECT
 date,
 home_goal,
@@ -47,6 +53,7 @@ SUM(home_goal)
       1 PRECEDING AND CURRENT ROW) AS last2_homegoals
 FROM match
 WHERE hometeam_id = 8456 AND season = '2011/2012';
+
 
 ---------------------------------------------------------------------------------------------
 
@@ -106,6 +113,3 @@ away AS (
 -------------------------------------------
 
 -- Created on 8.9.2025
-
-
-
